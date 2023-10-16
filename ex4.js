03e707337862657960192b1f57b0d01fb0da8d97
Albertina75 <145276835+Albertina75@users.noreply.github.com>
2023-10-16 13:18:55 +0200

    ex4.js contestado

--- ex4.js
+++ ex4.js
@@ -11,7 +11,7 @@
  console.log("Nemo está en la posición: " + pezPosicion)
 
  /**
-  * Fíjate sin embargo qué valor arroja aplicar indexOf cuando buscamos a "estrella". CONTESTA: ¿Por qué crees que sucede esto?
+  * Fíjate sin embargo qué valor arroja aplicar indexOf cuando buscamos a "estrella". CONTESTA: ¿Por qué crees que sucede esto? Esto sucede porque no puede encontrarlo y para indicar que no se encuentra la cadena en el array, utiliza el -1. Estrella y estrella son tratados como dos cadenas de texto diferentes por la distinción entre mayúsculas y minúsculas.
   */
 
   let pezPosicion2 = muchos_peces.indexOf("estrella")
